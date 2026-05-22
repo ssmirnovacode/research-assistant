510fba03907e0bae1f401b5bcb0c46deb82f73c1
ssmirnovacode <svetlana.smirnova213@gmail.com>
2026-05-22 18:02:27 +0200

    added initial message

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
-import { ChatArea } from '@/components/chat/ChatArea'
-import { INITIAL_MESSAGES } from '@/lib/mock-data'
+import { ChatArea } from "@/components/chat/ChatArea";
+import { INITIAL_MESSAGES } from "@/lib/constants";
 
 export default function Home() {
-  return <ChatArea initialMessages={INITIAL_MESSAGES} />
+  return <ChatArea initialMessages={INITIAL_MESSAGES} />;
 }
